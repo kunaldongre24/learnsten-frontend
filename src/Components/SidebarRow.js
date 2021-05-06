7cac8349ca09d07c7cce93bfdd61385e64d48908
kunaldongre24 <dongrekunal16@gmail.com>
2021-05-06 21:55:21 +0530

    Worked on profile.js

--- src/Components/SidebarRow.js
+++ src/Components/SidebarRow.js
@@ -2,13 +2,14 @@ import "../style/sidebarrow.css";
 import { NavLink } from "react-router-dom";
 function SidebarRow(props) {
   const { Title, Icon, Path } = props;
+  const iconStyle = { fontSize: "22px" };
   return (
     <>
       <NavLink to={{ pathname: Path }} exact activeClassName="selected">
         <div className="list-element">
           <div className="contain-element">
             <div className="list-icon">
-              <Icon />
+              <Icon style={iconStyle} />
             </div>
             <div className="list-desc">{Title}</div>
           </div>

0c47fdf0705c29203d1c5869eb1ea94d4912e688
kunaldongre24 <dongrekunal16@gmail.com>
2021-05-08 15:39:36 +0530

    scroll to top on switching components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
-import "./style/App.css";
-import "./style/index.css";
+import "../style/App.css";
+import "../style/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import Container from "./Components/Container";
+import Container from "./Container";
+import ScrollToTop from "./ScrolltoTop";
+
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Container />
       </Router>
     </div>
